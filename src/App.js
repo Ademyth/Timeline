2d15e8fbeea83b9716ba93be48690dbb3d0149e7
Ademyth <adepegbaadegoke@gmail.com>
2023-04-22 13:39:56 +0100

    time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as WorkIcon } from "./rocket.svg";
+import { ReactComponent as SchoolIcon } from "./sharingan.svg";
+
+import timelineElements from "./timelineElements";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
 
 function App() {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#CA1616" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="title">AdeMyth Timeline</h2>
+      <p className="short">Hey there, Human! This timeline is where I dump all my random musings and wacky shenanigans. It's like a big, fabulous playground for my brain in order not to forget.😁🤭</p>
+      <VerticalTimeline>
+        {timelineElements.map((element) => {
+          let isWorkIcon = element.icon === "work";
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {element.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+              <p id="description">{element.description}</p>
+              {showButton && (
+                <a
+                  className={`button ${
+                    isWorkIcon ? "workButton" : "schoolButton"
+                  }`}
+                  href="/"
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
     </div>
   );
 }
---
+++ src/timelineElements.js
@@ -0,0 +1,128 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Waking up to this reality...",
+    description:
+      "On this day, I woke up to reality—a day I can't remember, but I knew I cried because why Nigeria🥲💔",
+    date: "2001",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "print('Hello, World!')",
+    description:
+      "I started with Python and fell in love with it. And this is where it all began.🌚 And yeah I didn't start coding at the age of three, sad right?😭💔",
+    date: "2018",
+    icon: "school",
+  },
+  {
+    id: 3,
+    title: "Playing with Python",
+    description:
+      "Understood the basics of Python and built a simple whatsapp bot, face detection program, and many others I can't remember",
+    date: "2018",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Tasted Kotlin🤢🤮 and UNITY",
+    description:
+      "Built a Tic-Tac-Toe app that took me weeks to finish. Gradle crashed my system multiple times but it was worth it and at that moment I knew Kotlin wasn't meant for me. And also built one 3D game with UNITY",
+    date: "2018",
+    icon: "school",
+  },
+  {
+    id: 5,
+    title: "Where the real fun began",
+    description:
+      "Got passionate about Machine Learning and AI... Was also learning Web development(Html, CSS and JavaScript) and Wordpress",
+    date: "2019",
+    icon: "work",
+  },
+
+  {
+    id: 6,
+    title: "Machine Learning made me depressed",
+    description:
+      "Worked on my first model which I can't remember",
+    date: "2019",
+    icon: "school",
+  },
+
+  {
+    id: 8,
+    title: "Forexa and Crypto",
+    description:
+      "Started learning about Forex Trading and Crypto Trading",
+    date: "2019",
+    icon: "work",
+  },
+
+  {
+    id: 9,
+    title: "Byee Machine Learning",
+    description:
+      "Built a crypto trading bot with python using Alpaca trade api",
+    date: "2020",
+    icon: "school",
+  },
+
+  {
+    id: 10,
+    title: "Blew my first Forex Account",
+    description:
+      "Acually have nothing to say. But just know how to set your stop loss kids",
+    date: "2021",
+    icon: "work",
+  },
+
+  {
+    id: 11,
+    title: "5 Months of Depression",
+    description:
+      "Lost a lot(Would have loved to say the full story but Nah.)... Got depressed to the point I forgot some trading theories. TRUST NO HUMAN GUYS",
+    date: "2021",
+    icon: "school",
+  },
+
+  {
+    id: 12,
+    title: "My very first remote Internship",
+    description:
+      "Interned as a Typescript Backend Engineer at Softkave(A startup company)",
+    date: "2021",
+    icon: "work",
+  },
+
+
+  {
+    id: 13,
+    title: "Just had to REACT",
+    description:
+      "Started learning React and built my first chat web application using chatengine.io api",
+    date: "January - 2022",
+    icon: "school",
+  },
+
+  {
+    id: 14,
+    title: "Wordpress Gig",
+    description:
+      "Got my first wordpress gig to work on a Real Estate site",
+    date: "January - 2022",
+    icon: "work",
+  },
+
+  {
+    id: 15,
+    title: "React Gig",
+    description:
+      "Made my first six figures.",
+    date: "2022",
+    icon: "school",
+  },
+
+
+];
+
+export default timelineElements;
